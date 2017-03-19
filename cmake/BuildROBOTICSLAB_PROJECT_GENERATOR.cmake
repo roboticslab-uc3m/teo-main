55c8e242339c03a7b9f007c20780bd9999da06ce
jgvictores <jcgvicto@gmail.com>
2017-03-19 19:30:22 +0100

    add BuildROBOTICSLAB_PROJECT_GENERATOR.cmake

---
+++ cmake/BuildROBOTICSLAB_PROJECT_GENERATOR.cmake
@@ -0,0 +1,6 @@
+include(YCMEPHelper)
+
+ycm_ep_helper(ROBOTICSLAB_PROJECT_GENERATOR TYPE GIT
+              STYLE GITHUB
+              REPOSITORY roboticslab-uc3m/project-generator.git
+              TAG master)
