354789832631cccacb039ec79c656ce77f0e1554
jgvictores <jcgvicto@gmail.com>
2018-02-02 20:00:33 +0100

    update names

--- cmake/BuildTEO_DEVELOPER_MANUAL.cmake
+++ cmake/BuildTEO_DEVELOPER_MANUAL.cmake
@@ -1,6 +1,6 @@
 include(YCMEPHelper)
 
-ycm_ep_helper(TEO_SOFTWARE_MANUAL TYPE GIT
+ycm_ep_helper(TEO_DEVELOPER_MANUAL TYPE GIT
               STYLE GITHUB
-              REPOSITORY roboticslab-uc3m/teo-software-manual.git
+              REPOSITORY roboticslab-uc3m/teo-developer-manual.git
               TAG master)
