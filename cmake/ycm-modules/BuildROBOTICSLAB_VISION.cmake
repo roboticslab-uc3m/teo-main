97e3338930312777ce1f147d9a8f2e2ba3a46abb
Bartek Łukawski <bwmn.peter@gmail.com>
2018-03-15 14:26:19 +0100

    Add build modules for YCM dependencies, set them

--- cmake/ycm-modules/BuildROBOTICSLAB_VISION.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_VISION.cmake
@@ -1,6 +1,10 @@
 include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
 
 ycm_ep_helper(ROBOTICSLAB_VISION TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/vision.git
-              TAG master)
+              TAG master
+              DEPENDS YARP)
