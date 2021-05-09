4852ad7c311fd93c4ed70ccddec2ab909132efa1
Bartek Łukawski <bwmn.peter@gmail.com>
2021-05-09 20:16:30 +0200

    Review CMake code and interdependencies

--- cmake/ycm-modules/BuildROBOTICSLAB_PROJECT_GENERATOR.cmake
+++
@@ -1,6 +0,0 @@
-include(YCMEPHelper)
-
-ycm_ep_helper(ROBOTICSLAB_PROJECT_GENERATOR TYPE GIT
-              STYLE GITHUB
-              REPOSITORY roboticslab-uc3m/project-generator.git
-              TAG master)
--- cmake/ycm-modules/BuildROBOTICSLAB_VISION.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_VISION.cmake
@@ -2,10 +2,9 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
-find_or_build_package(COLOR_DEBUG QUIET)
 
 ycm_ep_helper(ROBOTICSLAB_VISION TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/vision.git
               TAG master
-              DEPENDS "YARP;COLOR_DEBUG")
+              DEPENDS YARP)
