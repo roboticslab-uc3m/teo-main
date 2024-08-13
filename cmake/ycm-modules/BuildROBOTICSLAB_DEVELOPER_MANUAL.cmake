390fc8990e51ede572b9db548c091983cb2ff4f5
Bartek Łukawski <PeterBowman@users.noreply.github.com>
2024-08-13 10:46:29 +0200

    Remove dependency on YARP for docs-related repos

--- cmake/ycm-modules/BuildROBOTICSLAB_DEVELOPER_MANUAL.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_DEVELOPER_MANUAL.cmake
@@ -1,10 +1,6 @@
 include(YCMEPHelper)
-include(FindOrBuildPackage)
-
-find_or_build_package(YARP QUIET)
 
 ycm_ep_helper(ROBOTICSLAB_DEVELOPER_MANUAL TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/developer-manual.git
-              TAG master
-              DEPENDS YARP)
+              TAG master)
